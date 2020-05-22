24293e8377f1b8f8567a923abcdd4203cea81a33
Terry Hanratta <terrynirv@gmail.com>
2020-05-21 21:57:15 -0500

    Third commit

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ let rollResults = document.querySelector('#total-results')
 let allRollsElement = document.querySelector('#all-rolls')
 const dieQuant = document.querySelector('#dice')
 rollButton.addEventListener("click", function () {
-    
-    const die = dieQuant.value
+        const die = dieQuant.value
     console.log(die)
     let counter = 1
     let rollResults = 0
@@ -15,25 +14,20 @@ rollButton.addEventListener("click", function () {
         dieRolls.push(numberRolled)
         counter += 1;
         rollResults = numberRolled +rollResults
-        console.log(rollResults)
-        
-        
+        console.log(rollResults)           
     }
-    
-        document.body.innerHTML += "Total: " + rollResults 
-       console.log(dieRolls)
-    
+    document.body.innerHTML += "Total: " + rollResults 
+    console.log(dieRolls)
 })
-showallButtin.addEventListener("click",function () {
+showallButtin.addEventListener("click", function () {
     let counter2 = 0
     console.log('button')
-    while (counter2 <= dieRolls.length) {
+    while (counter2 < dieRolls.length) {
         let newDiceString = '<li class="dice">' + dieRolls[counter2] + '</li>';
         allRollsElement.innerHTML += newDiceString;
         console.log(newDiceString)
         counter2 += 1;
     }
-    
 })
 /*const newDiceString = '<li class="dice">' + numberRolled + "</li>";
 allRollsElement.innerHTML += newDiceString;*/
