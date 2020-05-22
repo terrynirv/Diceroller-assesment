763101de73428596cfa246f75492c70592f7c98a
Terry Hanratta <terrynirv@gmail.com>
2020-05-21 21:29:27 -0500

    Second commit.

--- main.js
+++ main.js
@@ -9,21 +9,31 @@ rollButton.addEventListener("click", function () {
     const die = dieQuant.value
     console.log(die)
     let counter = 1
+    let rollResults = 0
     while (counter <= die) { 
         const numberRolled = Math.floor(Math.random() * 6) + 1;
         dieRolls.push(numberRolled)
         counter += 1;
+        rollResults = numberRolled +rollResults
+        console.log(rollResults)
         
-
         
     }
-    let rollResults = dieRolls.value
-    document.body.innerHTML += "Total" + rollResults 
-    console.log(dieRolls)
+    
+        document.body.innerHTML += "Total: " + rollResults 
+       console.log(dieRolls)
+    
 })
 showallButtin.addEventListener("click",function () {
-    const newDiceString = '<li class="dice">' + dieRolls + "</li>";
-allRollsElement.innerHTML += newDiceString;
+    let counter2 = 0
+    console.log('button')
+    while (counter2 <= dieRolls.length) {
+        let newDiceString = '<li class="dice">' + dieRolls[counter2] + '</li>';
+        allRollsElement.innerHTML += newDiceString;
+        console.log(newDiceString)
+        counter2 += 1;
+    }
+    
 })
 /*const newDiceString = '<li class="dice">' + numberRolled + "</li>";
 allRollsElement.innerHTML += newDiceString;*/
